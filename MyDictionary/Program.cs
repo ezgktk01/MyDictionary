0685dc6b7c361c777eabddf9df2b304226392d45
ezgi kütük <ezgikutuk3@gmail.com>
2021-01-23 14:53:13 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(01, "Adana");
+            myDictionary.Add(06, "Ankara");
+            myDictionary.Add(34, "İstanbul");
+            myDictionary.Add(35, "İzmir");
+
+            foreach (var name in myDictionary.Keys)
+            {
+                Console.WriteLine(name);
+            }
+            foreach (var number in myDictionary.Values)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
